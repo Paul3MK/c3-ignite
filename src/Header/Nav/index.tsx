8071cb528ccc561a7243912dffb677c0ae0f1106
Paul K <paulmkouadio@gmail.com>
2025-04-13 05:20:19 +0000

    feat(wip): make nav indicate current page

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -2,24 +2,32 @@
 
 import React from 'react'
 
-import type { Header as HeaderType } from '@/payload-types'
+import type { Header as HeaderType, Page } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 
 export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
+  const pathname = usePathname()
+
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex gap-3 items-center text-[21px]">
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
+        const l = link.reference?.value as Page
+        return (
+          <CMSLink
+            key={i}
+            {...link}
+            appearance="link"
+            className={clsx({ underline: l.slug == pathname.slice(1) })}
+          />
+        )
       })}
-      <Link href="/search">
-        <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
-      </Link>
     </nav>
   )
 }
