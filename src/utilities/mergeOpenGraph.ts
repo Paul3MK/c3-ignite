1eac5626a157fea09246c0462251b3e356e1adab
Paul K <paulmkouadio@gmail.com>
2025-04-15 11:22:14 +0000

    feat: setup meta attributes for SEO

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'A vibrant community of believers in Accra, Ghana.',
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'C3 Ignite Church',
+  title: 'C3 Ignite Church',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
