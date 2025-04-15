550937dcb5428fd7616817ee65738116a0cd04a1
Paul K <paulmkouadio@gmail.com>
2025-04-15 11:18:48 +0000

    feat: setup footer

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -21,13 +21,13 @@ export async function Footer() {
         </Link>
 
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
           <nav className="flex flex-col md:flex-row gap-4">
             {navItems.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
             })}
           </nav>
         </div>
+        <span>Copyright © {new Date().getUTCFullYear()} C3 Ignite</span>
       </div>
     </footer>
   )
