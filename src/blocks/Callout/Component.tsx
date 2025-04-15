47f2d518baf83f9bddc6082a0da059ca75971684
Paul K <paulmkouadio@gmail.com>
2025-04-15 11:22:37 +0000

    feat: tweak callout ui

--- src/blocks/Callout/Component.tsx
+++ src/blocks/Callout/Component.tsx
@@ -1,25 +1,33 @@
 import type { Callout as CalloutProps } from '@/payload-types'
 import { RichText } from '@payloadcms/richtext-lexical/react'
-import { ArrowUpRight } from 'lucide-react'
+import { ArrowRight, ArrowUpRight } from 'lucide-react'
 
 export const CalloutBlock: React.FC<CalloutProps> = (props) => {
   return (
-    <div style={{ backgroundColor: props.backgroundColor }} className="-mt-16">
-      <div className="container mx-auto py-64 relative grid grid-cols-12 grid-flow-col items-center">
-        <RichText
-          data={props.caption}
-          className="col-span-5 font-serif text-[157px] text-center text-white leading-[0.8] z-[1]"
-        />
-        <div className="flex flex-col col-span-5 col-start-8 gap-16">
-          <RichText data={props.summaryText} className="font-sans text-[21px] text-white" />
+    <div style={{ backgroundColor: props.backgroundColor }}>
+      <div className="container mx-auto py-32 md:py-64 relative grid gap-16 md:gap-0 grid-cols-4 md:grid-cols-12 grid-flow-row md:grid-flow-col items-center">
+        <div className="w-[50%] mx-auto col-span-full md:col-span-5 z-[1]">
+          <RichText
+            data={props.caption}
+            className=" font-serif text-[5.625rem] md:text-[10rem]  text-center text-white leading-[0.8]"
+          />
+        </div>
+        <div className="flex flex-col col-span-full md:col-span-5 md:col-start-8 gap-16">
+          <RichText
+            data={props.summaryText}
+            className="font-sans text-base md:text-[1.312rem] text-white"
+          />
           <div className="flex flex-col gap-5">
             {props.CTAs?.map((cta) => (
               <span
                 key={cta.id}
                 className="grid gap-3 grid-flow-row grid-cols-[auto_auto] justify-start items-center "
               >
-                <RichText data={cta.content!} className="text-white w-fit text-[28px] font-serif" />{' '}
-                <ArrowUpRight className="text-white ml-4" />
+                <RichText
+                  data={cta.content!}
+                  className="text-white w-fit text-[1.312rem] font-serif"
+                />{' '}
+                <ArrowRight className="text-white ml-4 text-sm" />
               </span>
             ))}
           </div>
