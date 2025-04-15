16c3ec40b875246ffe77a1636046ec137affb3d2
Paul K <paulmkouadio@gmail.com>
2025-04-15 11:25:57 +0000

    fix: adjust responsiveness and scale for intro section block

--- src/blocks/Intro/Component.tsx
+++ src/blocks/Intro/Component.tsx
@@ -4,17 +4,19 @@ import { RichText } from '@payloadcms/richtext-lexical/react'
 export const IntroSection: React.FC<IntroSectionProps> = (props) => {
   return (
     <div style={{ backgroundColor: props.backgroundColor }} className="-mt-16">
-      <div className="container mx-auto py-40 relative grid grid-cols-4 md:grid-cols-12 gap-20 md:gap-4 grid-flow-col items-center">
+      <div className="container mx-auto py-20 relative grid grid-cols-4 md:grid-cols-12 gap-20 md:gap-4 grid-flow-col items-center">
         <span
           style={{ color: props.accentColor, opacity: '10%' }}
-          className="font-headline text-[7.5rem] md:text-[13.3rem] absolute left-8 bottom-32 md:bottom-auto max-w-[42rem] my-auto leading-[0.7] uppercase"
+          className="font-headline text-[7.5rem] md:text-[13.3rem] absolute left-8 bottom-24 md:bottom-auto max-w-[42rem] my-auto leading-[0.7] uppercase"
         >
           {props.graphicText}
         </span>
-        <RichText
-          data={props.caption}
-          className="col-span-full md:col-span-5 font-serif text-[5.625rem] md:text-[10rem] text-center text-white leading-[0.8] z-[1]"
-        />
+        <div className="col-span-full md:col-span-5 w-[50%] mx-auto md:w-auto md:mx-0 z-[1]">
+          <RichText
+            data={props.caption}
+            className="font-serif text-[5.625rem] md:text-[10rem] text-center text-white leading-[0.8]"
+          />
+        </div>
         <RichText
           data={props.summaryText}
           className="col-span-full row-start-1 md:row-start-auto md:col-span-5 md:col-start-8 font-sans text-base md:text-[1.312rem] leading-snug text-white"
